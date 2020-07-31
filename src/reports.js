1e6e5660c721d3b99c2cf8ef8f4e5175a74cdea6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2020-07-31 14:38:43 +0200

    update r2rml4net reports

--- src/reports.js
+++ src/reports.js
@@ -1,4 +1,4 @@
 export default [
-  'https://r2rml4net.github.io/test-runner/earl-r2rml4net-dm.ttl',
-  'https://r2rml4net.github.io/test-runner/earl-r2rml4net-r2rml.ttl',
+  'https://r2rml4net.github.io/r2rml4net-implementation-report/earl-r2rml4net-dm.ttl',
+  'https://r2rml4net.github.io/r2rml4net-implementation-report/earl-r2rml4net-r2rml.ttl',
 ];
